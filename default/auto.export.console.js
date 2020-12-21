bc6088c180f8314cd51153ae37904596d8103f3f
PlanC <planc2333@outlook.com>
2020-12-21 12:58:32 +0800

    update auto.export.console.js

--- default/auto.export.console.js
+++ default/auto.export.console.js
@@ -1,8 +1,8 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-20 13:58:36
- * @LastEditTime: 2020-08-20 14:09:02
- * @FilePath: \Screeps\auto.export.console.js
+ * @LastEditTime: 2020-12-21 12:58:19
+ * @FilePath: \Screeps\default\auto.export.console.js
  */
 
 var oldTime = new Date().getTime();
@@ -13,26 +13,26 @@ var consoleExport = {
 	run: function(marketMode) {
 
 		newTime = new Date().getTime();
-		console.log('Game Time is: <font color="cyan">' + Game.time + '</font> ' + '(<font color="cyan">' + ((newTime - oldTime) / 1000) + '</font>second).');
+		console.log('Game Time is: <span style="color:#FF0000;">' + Game.time + '</span> ' + '(<span style="color:#FF0000;">' + ((newTime - oldTime) / 1000) + '</span>second).');
 		oldTime = newTime;
 
-		console.log('You have <font color="cyan">' + Game.market.credits + '</font> Credits aviliable.');
+		console.log('You have <span style="color:#FF0000;">' + Game.market.credits + '</span> Credits aviliable.');
 
 		for (var name in Game.rooms) {
 			if (Game.rooms[name].controller == null) return;
-			console.log('Room "<font color="cyan">' + name + '</font>' +
-						'" has <font color="cyan">' + Game.rooms[name].energyAvailable + '</font>/<font color="cyan">' + Game.rooms[name].energyCapacityAvailable + '</font> energy' +
-						', controller level is <font color="cyan">' + Game.rooms[name].controller.level + '</font>' +
-						', progress left <font color="cyan">' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '</font>.\t');
+			console.log('Room "<span style="color:#FF0000;">' + name + '</span>' +
+						'" has <span style="color:#FF0000;">' + Game.rooms[name].energyAvailable + '</span>/<span style="color:#FF0000;">' + Game.rooms[name].energyCapacityAvailable + '</span> energy' +
+						', controller level is <span style="color:#FF0000;">' + Game.rooms[name].controller.level + '</span>' +
+						', progress left <span style="color:#FF0000;">' + (Game.rooms[name].controller.progressTotal - Game.rooms[name].controller.progress) + '</span>.\t');
 		}
 
-	    // console.log('Room "' + 'W34N2' + '" has <font color="cyan">' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</font> Energy to use, market mode is <font color="cyan">' + marketMode + '</font>.');
-		// console.log('Room "' + 'W34N3' + '" has <font color="cyan">' + Game.rooms['W34N3'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</font> Energy to use, market mode is <font color="cyan">' + marketMode + '</font>.');
+	    // console.log('Room "' + 'W34N2' + '" has <span style="color:#FF0000;">' + Game.rooms['W34N2'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</span> Energy to use, market mode is <span style="color:#FF0000;">' + marketMode + '</span>.');
+		// console.log('Room "' + 'W34N3' + '" has <span style="color:#FF0000;">' + Game.rooms['W34N3'].terminal.store.getUsedCapacity(RESOURCE_ENERGY) + '</span> Energy to use, market mode is <span style="color:#FF0000;">' + marketMode + '</span>.');
 
 		for (var name in Memory.creeps) {
 			if (!Game.creeps[name]) {
 				delete Memory.creeps[name];
-				console.log('Clearing non-existing creep memory:<font color="cyan">' +  name + '</font>.');
+				console.log('Clearing non-existing creep memory:<span style="color:#FF0000;">' +  name + '</span>.');
 			}
 		}
 	}
